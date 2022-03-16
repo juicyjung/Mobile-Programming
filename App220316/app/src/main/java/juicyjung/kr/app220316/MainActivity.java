b8e1b39cd68e410a6d4ad7045a0a78503dc586b9
juicyjung <travelandi01@gmail.com>
2022-03-16 15:12:51 +0900

    220316 ex2

--- App220316/app/src/main/java/juicyjung/kr/app220316/MainActivity.java
+++ App220316/app/src/main/java/juicyjung/kr/app220316/MainActivity.java
@@ -1,4 +1,4 @@
-package kr.juicyjung.myfirstapplication;
+package juicyjung.kr.app220316;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R    .layout.activity_main);
+        setContentView(R.layout.activity_main);
     }
 }
