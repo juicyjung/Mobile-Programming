283f09ac2b17f322e926c95fc0f321a7b476cb9d
juicyjung <travelandi01@gmail.com>
2022-03-18 17:25:45 +0900

    for upload folder

---
+++ App220316/app/build/generated/source/buildConfig/debug/juicyjung/kr/app220316/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package juicyjung.kr.app220316;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "juicyjung.kr.app220316";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
